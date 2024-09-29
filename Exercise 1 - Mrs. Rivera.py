6104a1aac9f9359883ed5a0f5df9f6a39e6aac7e
unknown <centinaraphael0157@gmail.com.>
2024-09-29 20:53:25 +0800

    code validation upload

--- Exercise 1 - Mrs. Rivera.py
+++ Exercise 1 - Mrs. Rivera.py
@@ -1,12 +1,21 @@
 def raise_to(base, power):
+    if power < 0:
+        raise ValueError("Exponent should be a non-negative integer.")
     answer = base
     if power == 0:
         return 1
     elif power == 1:
         return answer
     else:
-        for _ in range (1,power):
-            answer = answer*base
+        for _ in range(1, power):
+            answer = answer * base
         return answer
 
-print(raise_to(4,4))
+try:
+    user_base = int(input("What is your base? "))
+    user_exponent = int(input('What number should we raise it to? '))
+
+    print(raise_to(user_base, user_exponent))
+
+except ValueError:
+    print("Please enter an integer value for both the base and the exponent.")
