343f4a4d7bce80249a88a94e4aa509a65a30cc13
unknown <centinaraphael0157@gmail.com.>
2024-09-29 21:46:24 +0800

    input validation

--- Exercise 4 - Inverted triangle maker.py
+++ Exercise 4 - Inverted triangle maker.py
@@ -1,3 +1,9 @@
-user_n_input = int(input("How high do you want your triangle to be: "))
-for i in range (1,user_n_input,-1):
-    print("*"*(i))
+def right_triangle_printer():
+    try: 
+        user_n_input = int(input("How high do you want your triangle to be: "))
+        for i in range (user_n_input,1,-1):
+            print("*"*(i))
+    except ValueError:
+        print("Please only input")
+        right_triangle_printer()
+right_triangle_printer()
